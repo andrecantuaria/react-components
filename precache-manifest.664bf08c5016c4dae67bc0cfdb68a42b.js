04fed3dd78dd7288d6a5fc493d37bd263e7365b5
Andre Cantuaria <andrecantuaria@gmail.com>
2024-06-01 11:26:01 -0500

    Deploy React app to GitHub Pages

---
+++ precache-manifest.664bf08c5016c4dae67bc0cfdb68a42b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb64f15b0b0e1c2464fbe7a43673126",
+    "url": "/andrecantuaria/react-components.git/index.html"
+  },
+  {
+    "revision": "33019000309ae73706c0",
+    "url": "/andrecantuaria/react-components.git/static/css/main.adf9b6f5.chunk.css"
+  },
+  {
+    "revision": "d487137f422adc6032be",
+    "url": "/andrecantuaria/react-components.git/static/js/2.6f448168.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/andrecantuaria/react-components.git/static/js/2.6f448168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120bb406e0d5715f62eb",
+    "url": "/andrecantuaria/react-components.git/static/js/3.1940e363.chunk.js"
+  },
+  {
+    "revision": "33019000309ae73706c0",
+    "url": "/andrecantuaria/react-components.git/static/js/main.8a25d472.chunk.js"
+  },
+  {
+    "revision": "b3bc9dac895c8e385336",
+    "url": "/andrecantuaria/react-components.git/static/js/runtime-main.a835cf8a.js"
+  },
+  {
+    "revision": "4e0c615eb181b8bec9e64dfd0d2a6431",
+    "url": "/andrecantuaria/react-components.git/static/media/img1.4e0c615e.jpg"
+  },
+  {
+    "revision": "6b7a518daa280351bd3b5f7dce1e518d",
+    "url": "/andrecantuaria/react-components.git/static/media/img2.6b7a518d.jpg"
+  },
+  {
+    "revision": "a03640aa8bf8f85f83227ca777e96460",
+    "url": "/andrecantuaria/react-components.git/static/media/img3.a03640aa.jpg"
+  },
+  {
+    "revision": "2d3c7f6ff91e55f609ee60a7a419a39a",
+    "url": "/andrecantuaria/react-components.git/static/media/img4.2d3c7f6f.jpg"
+  }
+]);
